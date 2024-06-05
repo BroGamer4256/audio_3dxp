8faa1f28d0c299b94094c6008075579dc1688cc2
BroGamer <64546358+BroGamer4256@users.noreply.github.com>
2024-06-05 14:46:57 +1200

    Use own hook.rs

--- src/hook.rs
+++ src/hook.rs
@@ -1,8 +1,27 @@
-#![allow(improper_ctypes)]
-#[link(name = "wal_3dxp", kind = "dylib")]
-extern "C" {
-	pub fn hook_symbol(symbol: &str, func: *const ()) -> *const ();
-	pub fn get_symbol(symbol: &str) -> *mut ();
-	pub fn hook(address: *mut (), func: *const ()) -> *const ();
-	pub fn write_memory(address: *mut (), data: &[u8]);
+use libc::*;
+use std::ffi::CString;
+
+pub unsafe fn hook_symbol(symbol: &str, func: *const ()) -> *const () {
+	hook(get_symbol(symbol), func)
+}
+
+pub unsafe fn get_symbol(symbol: &str) -> *mut () {
+	let symbol = CString::new(symbol).unwrap();
+	let module = dlopen(std::ptr::null(), RTLD_LAZY);
+	let address = dlsym(module, symbol.as_ptr());
+	dlclose(module);
+	address as *mut ()
+}
+
+pub unsafe fn hook(address: *mut (), func: *const ()) -> *const () {
+	let hook = retour::RawDetour::new(address, func).unwrap();
+	hook.enable().unwrap();
+	let trampoline = hook.trampoline() as *const ();
+	std::mem::forget(hook);
+	trampoline
+}
+
+pub unsafe fn write_memory(address: *mut (), data: &[u8]) {
+	region::protect(address, data.len(), region::Protection::READ_WRITE_EXECUTE).unwrap();
+	std::ptr::copy_nonoverlapping(data.as_ptr(), address as *mut u8, data.len());
 }
